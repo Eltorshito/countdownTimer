1cb20ead9f1976d42f0c7be1be92faee0fb4d78d
Eltorshito <cedricg697@gmail.com>
2023-11-08 14:57:15 +0100

    Update scripts-START.js

--- scripts-START.js
+++ scripts-START.js
@@ -11,10 +11,30 @@ function timer(seconds) {
     
 }
 
-function displayTimeLeft(timestamp) {
+function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds/60);
     const remainderSeconds = seconds % 60;
     const display = `*${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
     timerDisplay.textContent = display;
 
 }
+
+function displayEndTime (timestamp) {
+    const end = new date(timestamp)
+
+    let hours = end.getHours();
+    const minutes = end.getMinutes();
+
+    const adjustedHours = hours > 12 ? hours - 12 : hours;
+    const amPm = hours >= 12 ? 'PM' : 'AM';
+
+    const formattedTime = `${adjustedHours}:${minutes < 10 ? '0' : ''}${minutes} ${amPm}`;
+    endTime.textContent = `Fin à ${formattedTime}`;
+}
+
+buttons.forEach(buttons => {
+    buttons.addEventListener('click', function(){
+        const seconds = parseInt(this.dataset.time);
+        startTimer(seconds);
+    });
+});
